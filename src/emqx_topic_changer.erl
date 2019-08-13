ac77bbc335c89a039e9d57c83c62d2f69b3ebe1d
jdavidagudelo <jdaaa2009@gmail.com>
2019-08-13 14:03:35 -0500

    Fixing some issues.

--- src/emqx_topic_changer.erl
+++ src/emqx_topic_changer.erl
@@ -15,7 +15,7 @@
 %% API
 -export([set_topic/2]).
 
--spec(set_topic(emqx:topic(), emqx_types:message()) -> emqx_types:message()).
+-spec(set_topic(emqx:topic(), emqx_types:retained()) -> emqx_types:retained()).
 set_topic(Topic, Msg) ->
     NewTopic = re:replace(Topic, "/users/[^/]+","", [{return,list}]),
-    Msg#message{topic = NewTopic}.
+    Msg#retained{topic = NewTopic}.
