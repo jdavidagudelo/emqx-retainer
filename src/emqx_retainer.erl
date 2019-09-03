537f04f09a3ee7ab9b851dd4ef811279adbfd121
jdavidagudelo <jdaaa2009@gmail.com>
2019-09-03 10:42:01 -0500

    Removing log.

--- src/emqx_retainer.erl
+++ src/emqx_retainer.erl
@@ -213,7 +213,6 @@ code_change(_OldVsn, State, _Extra) ->
 dispatch_retained(_Topic, []) ->
     ok;
 dispatch_retained(Topic, Msgs) ->
-    ?LOG(error, "[Retainer] Unexpected info: ~p", [Topic]),
     NewMsgs = lists:flatmap(fun(Msg1)->[emqx_retainer_topic_changer:set_topic(Topic, Msg1)] end, sort_retained(Msgs)),
     [self() ! {deliver, Topic, Msg} || Msg  <- NewMsgs].
 
